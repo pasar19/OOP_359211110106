0e8f8ecc383ff9fa0bc78e2466046a1fb596d452
LABCOM_6 <diizyozee@gmail.com>
2017-11-17 11:15:26 +0700

    sucessfully infermation data

---
+++ src/oop_lab359211110106/must/com/myFirstAVA.java
@@ -0,0 +1,12 @@
+package oop_lab359211110106.must.com;
+public class myFirstAVA {
+
+    public static void main(String[] args) {
+     System.out.println("Hello World, I'm KIK");
+     System.out.println("Information System");
+     System.out.println("Faculty of Management Technology");
+     System.out.println("RMUTSV");
+    }//main
+}//class
+
+
